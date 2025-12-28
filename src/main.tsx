a163d2bc64e3ae5fa3f9ef64f3f57239eb563e54
indrajitkarmakar1209-debug <indrajitkarmakar1209@gmail.com>
2025-12-29 00:32:34 +0530

    Render Login screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Login from "./data/components/Login";
+import Login from "./components/Login";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
