f0e6ae19de6443954d1e3d53b870031b0a8adc94
indrajitkarmakar1209-debug <indrajitkarmakar1209@gmail.com>
2025-12-29 00:23:08 +0530

    Render Login screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>QC Productivity App</h1>
-      <p>App successfully deployed 🎉</p>
-    </div>
-  );
-}
+import Login from "./data/components/Login";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
